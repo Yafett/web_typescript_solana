9a66c49147def4981452de836944e97387aa06c1
Yafett <exdeath350@gmail.com>
2022-07-25 10:03:11 +0700

    'initialite'

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import './App.css';
 import MintNft from './MinNft';
 import MintToken from './MintToken';
 import SendSol from './SendSol';
+import { ImagePick } from './ImagePicker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MintToken />
-        <MintNft />
-        <SendSol />
+        <div className='divider'>
+          <h4>TOKEN</h4>
+          <MintToken />
+          <MintNft />
+          <SendSol />
+        </div>
+        <div className='divider'>
+          <h4>IMAGE</h4>
+          <ImagePick />
+        </div>
       </header>
     </div>
   );
